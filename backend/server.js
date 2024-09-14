9042c5d41fa0bc9df42541b3022d2082541c6930
Your Name <sujalverdhen1234@gmail.com>
2024-09-14 10:03:55 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,13 @@ import authRoutes from "./routes/auth.routes.js"
 import portfolioRoutes from "./routes/portfolio.routes.js"
 import connect from "./db/db.js"
 import cookieParser from "cookie-parser";
+import path from "path"
 const app=express();
 app.use(cookieParser())
 app.use(express.json())
+
 doenv.config();
+const __dirname=path.resolve()
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_KEY,
@@ -27,6 +30,10 @@ app.use(cors({ origin: 'http://localhost:3000',credentials: true}));
 
 app.use("/api/auth",authRoutes)
 app.use("/api/project",portfolioRoutes)
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 app.listen(5000,async()=>{
     await connect()
     console.log("port running on 5000")
