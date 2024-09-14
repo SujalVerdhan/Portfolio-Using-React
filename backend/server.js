1bbfaf77dbe9ad366cf2c9f5019c2582394aea91
Your Name <sujalverdhen1234@gmail.com>
2024-09-14 10:43:37 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.use(cors({ origin: 'http://localhost:3000',credentials: true}));
 
 app.use("/api/auth",authRoutes)
 app.use("/api/project",portfolioRoutes)
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.use(express.static(path.join(__dirname,"/frontend/build")))
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"frontend","build","index.html"))
 })
 app.listen(5000,async()=>{
     await connect()
